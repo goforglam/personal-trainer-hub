d9fd3767f45a9264d7b1ecad860d8f65e9c5a843
goforglam <8995ks4knr@privaterelay.appleid.com>
2026-09-21 15:50:53 +0200

    Add app/exercises/page.tsx

---
+++ app/exercises/page.tsx
@@ -0,0 +1,4 @@
+import { Section } from "../section";
+export default function ExercisesPage() {
+  return <Section title="Exercises" text="A future movement library with cues, regressions, progressions and coaching notes." />;
+}
