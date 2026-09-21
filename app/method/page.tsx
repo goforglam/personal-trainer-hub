7bb073022013fcb8eccbf9b3016f6019a83749ec
goforglam <8995ks4knr@privaterelay.appleid.com>
2026-09-21 15:50:58 +0200

    Add app/method/page.tsx

---
+++ app/method/page.tsx
@@ -0,0 +1,4 @@
+import { Section } from "../section";
+export default function MethodPage() {
+  return <Section title="Method" text="Your methodology will become the reusable intelligence behind assessments, programming, progression and adaptation." />;
+}
